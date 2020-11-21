1aa3856321263834006844a7e975ab0407c43fd1
djordjeognjenovic97 <djordjeognjenovic997@gmail.com>
2020-11-21 16:42:05 +0100

    Inicijalna postavka projekta, sa bazom i dependency-ima
    Napravljen inicijalni projekat, sa postgres bazom, potrebnim dependency-ima i paketima.

---
+++ culturalcontent/src/main/java/com/tim5/culturalcontent/CulturalcontentApplication.java
@@ -0,0 +1,13 @@
+package com.tim5.culturalcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturalcontentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulturalcontentApplication.class, args);
+	}
+
+}
