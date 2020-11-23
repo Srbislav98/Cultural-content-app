bc11ac8ccd0767ca1e5c40dfb26d8f20a8a3b267
sw792017 <52822584+sw792017@users.noreply.github.com>
2020-11-23 17:23:14 +0100

    TipKulturnePonude obj (bez veze)

---
+++ culturalcontent/src/main/java/com/tim5/culturalcontent/model/TipKulturnePonude.java
@@ -0,0 +1,40 @@
+package com.tim5.culturalcontent.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name= "TIPKULTURNEPONUDE")
+@Inheritance(strategy= InheritanceType.TABLE_PER_CLASS)
+public abstract class TipKulturnePonude {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//@Column(nullable=false, unique = true)//
+    private Integer id;
+
+    @Column(nullable=false, unique = true)
+    private String naziv;
+
+    public TipKulturnePonude() {
+    }
+
+    public TipKulturnePonude(Integer id, String naziv) {
+        this.id = id;
+        this.naziv = naziv;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+}
