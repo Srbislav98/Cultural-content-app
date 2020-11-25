c79397bcf8841d7228a8eced4225d10176e26f18
sw792017 <52822584+sw792017@users.noreply.github.com>
2020-11-25 20:53:29 +0100

    veza sa bazom

--- Cultural-content/src/main/java/com/kts/cultural_content/model/KulturnaPonuda.java
+++ Cultural-content/src/main/java/com/kts/cultural_content/model/KulturnaPonuda.java
@@ -1,7 +1,10 @@
 package com.kts.cultural_content.model;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 
@@ -26,16 +29,66 @@ public abstract class KulturnaPonuda {
     @Column(nullable=false)
     private String opis;
 
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<Fotografija> fotogrfija = new HashSet<>();
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn( nullable = false)
+    private Admin admin;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn( nullable = false)
+    private TipKulturnePonude tipKulturnePonude;
+
+    @ManyToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    private List<RegistrovaniKorisnik> registrovaniKorisnik;
+
     public KulturnaPonuda() {
     }
 
-    public KulturnaPonuda(Integer id, String naziv, String geoSirina, String geoDuzina, String adresa, String opis) {
+    public Admin getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Admin admin) {
+        this.admin = admin;
+    }
+
+    public TipKulturnePonude getTipKulturnePonude() {
+        return tipKulturnePonude;
+    }
+
+    public void setTipKulturnePonude(TipKulturnePonude tipKulturnePonude) {
+        this.tipKulturnePonude = tipKulturnePonude;
+    }
+
+    public List<RegistrovaniKorisnik> getRegistrovaniKorisnik() {
+        return registrovaniKorisnik;
+    }
+
+    public void setRegistrovaniKorisnik(List<RegistrovaniKorisnik> registrovaniKorisnik) {
+        this.registrovaniKorisnik = registrovaniKorisnik;
+    }
+
+    public Set<Fotografija> getFotogrfija() {
+        return fotogrfija;
+    }
+
+    public void setFotogrfija(Set<Fotografija> fotogrfija) {
+        this.fotogrfija = fotogrfija;
+    }
+
+    public KulturnaPonuda(Integer id, String naziv, String geoSirina, String geoDuzina, String adresa, String opis, Admin admin, TipKulturnePonude tipKulturnePonude) {
         this.id = id;
         this.naziv = naziv;
         this.geoSirina = geoSirina;
         this.geoDuzina = geoDuzina;
         this.adresa = adresa;
         this.opis = opis;
+        this.admin = admin;
+        this.tipKulturnePonude = tipKulturnePonude;
+        this.fotogrfija = new HashSet<>();
+        this.registrovaniKorisnik = new ArrayList<RegistrovaniKorisnik>();
     }
 
     public Integer getId() {
