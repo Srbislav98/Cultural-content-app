211bafb07fee36934b030e826aa641b5e0758264
JovanCorilic <kssbc39@gmail.com>
2020-12-02 21:21:00 +0100

    uradjeni CRUD za komentar

--- Cultural-content/src/main/java/com/kts/cultural_content/repository/KomentarRepository.java
+++ Cultural-content/src/main/java/com/kts/cultural_content/repository/KomentarRepository.java
@@ -1,7 +1,16 @@
 package com.kts.cultural_content.repository;
 
 import com.kts.cultural_content.model.Komentar;
+import com.kts.cultural_content.model.KulturnaPonuda;
+import com.kts.cultural_content.model.RegistrovaniKorisnik;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface KomentarRepository extends JpaRepository<Komentar, Integer> {
+
+    Komentar findByRegistrovaniKorisnik(RegistrovaniKorisnik registrovaniKorisnik);
+
+    Komentar findByKulturnaPonuda(KulturnaPonuda kulturnaPonuda);
+
+    Komentar findByVrednost(String vrednost);
+
 }
--- Cultural-content/src/main/java/com/kts/cultural_content/service/KomentarService.java
+++ Cultural-content/src/main/java/com/kts/cultural_content/service/KomentarService.java
@@ -1,32 +1,55 @@
 package com.kts.cultural_content.service;
 
 import com.kts.cultural_content.model.Komentar;
+import com.kts.cultural_content.model.Ocena;
+import com.kts.cultural_content.repository.KomentarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class KomentarService implements ServiceInterface<Komentar> {
+    @Autowired
+    private KomentarRepository komentarRepository;
+
     @Override
     public List<Komentar> findAll() {
-        return null;
+        return komentarRepository.findAll();
     }
 
     @Override
     public Komentar findOne(Integer id) {
-        return null;
+        return komentarRepository.findById(id).orElse( null);
     }
 
     @Override
     public Komentar create(Komentar entity) throws Exception {
-        return null;
+        return komentarRepository.save(entity);
     }
 
     @Override
     public Komentar update(Komentar entity, Integer id) throws Exception {
-        return null;
+        Komentar existingkomentar = komentarRepository.findById(id).orElse(null);
+        if(existingkomentar == null){
+            throw new Exception("komentar with given id doesn't exist");
+        }
+        existingkomentar.setVrednost(entity.getVrednost());
+        existingkomentar.setKulturnaPonuda(entity.getKulturnaPonuda());
+        existingkomentar.setRegistrovaniKorisnik(entity.getRegistrovaniKorisnik());
+
+        return komentarRepository.save(existingkomentar);
     }
 
     @Override
     public void delete(Integer id) throws Exception {
+        Komentar existingkomentar = komentarRepository.findById(id).orElse(null);
+        if (existingkomentar == null){
+            throw new Exception("komentar with given id doesn't exist");
+        }
+        komentarRepository.delete(existingkomentar);
+    }
 
+    public Komentar save(Komentar o){
+        return komentarRepository.save(o);
     }
 }
