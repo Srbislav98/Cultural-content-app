9aadc156725eaf99f2440fa20fcc7654f05fa8ab
djordjeognjenovic97 <djordjeognjenovic997@gmail.com>
2020-12-03 09:58:07 +0100

    Ispravke za admina i reg korisnika

--- Cultural-content/src/main/java/com/kts/cultural_content/repository/AdminRepository.java
+++ Cultural-content/src/main/java/com/kts/cultural_content/repository/AdminRepository.java
@@ -4,5 +4,6 @@ import com.kts.cultural_content.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends  JpaRepository<Admin, Integer>{
-
+    Admin findByKorisnickoIme(String korisnickoIme);
+    Admin findByKorisnickoImeAndIdNot(String korisnickoIme, Integer id);
 }
--- Cultural-content/src/main/java/com/kts/cultural_content/repository/RegistrovaniKorisnikRepository.java
+++ Cultural-content/src/main/java/com/kts/cultural_content/repository/RegistrovaniKorisnikRepository.java
@@ -4,4 +4,6 @@ import com.kts.cultural_content.model.RegistrovaniKorisnik;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RegistrovaniKorisnikRepository extends JpaRepository<RegistrovaniKorisnik, Integer> {
+    RegistrovaniKorisnik findByKorisnickoIme(String korisnickoIme);
+    RegistrovaniKorisnik findByKorisnickoImeAndIdNot(String korisnickoIme, Integer id);
 }
