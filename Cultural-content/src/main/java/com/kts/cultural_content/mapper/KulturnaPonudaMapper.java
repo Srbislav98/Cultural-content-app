f268628ef55d0e5fb1879086a94ed8bf4ab3d38b
sw792017 <52822584+sw792017@users.noreply.github.com>
2020-12-16 20:33:46 +0100

    Zadaci #8.3.1 #8.3.2
    Allow users to pick a type of cultural content while creating/editing a cultural content object #8.3.1
    
    Admin that creates cultural content automatically gets ownership over it #8.3.2

--- Cultural-content/src/main/java/com/kts/cultural_content/mapper/KulturnaPonudaMapper.java
+++ Cultural-content/src/main/java/com/kts/cultural_content/mapper/KulturnaPonudaMapper.java
@@ -3,17 +3,39 @@ import com.kts.cultural_content.dto.KulturnaPonudaDTO;
 import com.kts.cultural_content.mapper.MapperInterface;
 import com.kts.cultural_content.model.*;
 import com.kts.cultural_content.repository.TipKPRepository;
+import com.kts.cultural_content.service.TipKPService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class KulturnaPonudaMapper implements MapperInterface<KulturnaPonuda, KulturnaPonudaDTO> {
     @Autowired
-    private TipKPRepository oTip;
+    private TipKPService oTip;
 
     @Override
     public KulturnaPonuda toEntity(KulturnaPonudaDTO dto) {
-        return new KulturnaPonuda(dto.getId(), dto.getNaziv(), dto.getGeoSirina(), dto.getGeoDuzina(), dto.getAdresa(), dto.getOpis(), dto.getIdt());
+        System.out.println("#toEntity###########################################");
+        Object admin = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        Admin ad = (Admin) admin;
+
+        System.out.println("Admin : ");
+        System.out.println("\tID - " + ad.getId());
+        System.out.println("\tNaziv - " + ad.getIme());
+        System.out.println("\tEmail - " + ad.getEmail());
+        System.out.println("-----------------------------------------------------");
+
+        TipKulturnePonude tip = new TipKulturnePonude(1, dto.getIdt().toString());
+
+        System.out.println("Tip KP : ");
+        System.out.println("\tID - " + tip.getId());
+        System.out.println("\tNaziv - " + tip.getNaziv());
+        System.out.println("-----------------------------------------------------");
+
+
+        return new KulturnaPonuda(dto.getId(), dto.getNaziv(), dto.getGeoSirina(), dto.getGeoDuzina(), dto.getAdresa(), dto.getOpis(), tip, ad);
     }
 
     @Override
