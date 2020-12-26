be0e73424d8e5107f342a9f0fe99f7a571e0f5ef
djordjeognjenovic97 <djordjeognjenovic997@gmail.com>
2020-12-26 08:41:06 +0100

    Uradjeni svi potrebni testovi za lokaciju i dodato jos testova za novosti

---
+++ Cultural-content/src/test/java/com/kts/cultural_content/constants/LokacijaConstants.java
@@ -0,0 +1,10 @@
+package com.kts.cultural_content.constants;
+
+public class LokacijaConstants {
+
+    public static final String DB_LOKACIJA_NAZIV = "Novi Sad";
+    public static final Integer DB_LOKACIJA_ID = 100;
+
+    public static final long FIND_ALL_NUMBER_OF_ITEMS = 1;
+
+}
